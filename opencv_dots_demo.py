2499b699b2cce4d1c38661658f4428d301dbe3ed
oliverweinm <oliver.weinmeier@stud.unibas.ch>
2022-07-15 17:37:17 +0200

    Refactor code background mask removal into function

--- opencv_dots_demo.py
+++ opencv_dots_demo.py
@@ -16,21 +16,14 @@ ALGORITHM
 -----------------------------------------------
 """
 
-dirImagesList = [imageName for imageName in listdir() if ".tiff" in imageName]
-print(dirImagesList)
-
-
-for i in range(0,len(dirImagesList)):
-  #img_unchanged = cv2.imread(dirImagesList[0], cv2.IMREAD_ANYDEPTH)
-  img_unchanged = cv2.imread(dirImagesList[i], cv2.IMREAD_UNCHANGED)
+def getImgInfo(source_img):
   cv2.imshow("img_unchanged", img_unchanged)
   h,w= img_unchanged.shape
   print(f"Original Height and Width: {h}x{w}, dtype: {img_unchanged.dtype}")
 
-  img_grayscale = cv2.imread(dirImagesList[i], cv2.IMREAD_GRAYSCALE)
-  cv2.imshow("img_grayscale", img_grayscale)
-  print(img_grayscale.dtype)
+  return(h,w)
 
+def removeBackground(source_img, thres, maxVal):
   """
   BINARY THRESHOLD ALGORITHM:
   if (src(x,y) > thresh):
@@ -38,28 +31,35 @@ for i in range(0,len(dirImagesList)):
   else:
     dst(x,y) = 0
   """
-  print(img_unchanged)
-  threshold = 5000
-  maxValue = 65535
-  th, result = cv2.threshold(img_unchanged, threshold, maxValue, cv2.THRESH_BINARY)
-
-  cv2.imshow("img_thresholded", result)
-
+  th, result = cv2.threshold(source_img, thres, maxVal, cv2.THRESH_BINARY)
+  cv2.imshow("img_threshold", result)
   """
   ALPHA BLEND MASK
-
-  image_blended = cv2.bitwise_and(img_unchanged, result)
-  bgr = image_blended[:,:3]
-  gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
-  bgr = icv2.cvtColor(gray ,cv2.COLOR_GRAY2BGR)
-  alpha = src[:,3]
-  image_blended = np.dstack([bgr, alpha])
   """
-
-  img_3gray = cv2.merge((img_unchanged, img_unchanged, img_unchanged, result))
-  cv2.imshow("im_3gray", img_3gray)
-
+  img_3gray = cv2.merge((source_img, source_img, source_img, result))
+  cv2.imshow("im_3grayscales_thresholded", img_3gray)
+  #Save image
   cv2.imwrite(f"{dirImagesList[0]}-cleaned.tiff", img_3gray)
+
+
+dirImagesList = [imageName for imageName in listdir() if ".tiff" in imageName]
+print(dirImagesList)
+
+
+for i in range(len(dirImagesList)):
+  #img_unchanged = cv2.imread(dirImagesList[0], cv2.IMREAD_ANYDEPTH)
+  img_unchanged = cv2.imread(dirImagesList[i], cv2.IMREAD_UNCHANGED)
+  height,width = getImgInfo(img_unchanged)
+  img_grayscale = cv2.imread(dirImagesList[i], cv2.IMREAD_GRAYSCALE)
+  cv2.imshow("img_grayscale", img_grayscale)
+
+  #print(img_grayscale.dtype) #DEBUG String, uncomment to see the numpy array representation of the picture
+  #print(img_unchanged)       #DEBUG String, uncomment to see the numpy array representation of the picture
+
+  threshold = 5000
+  maxValue = 65535
+
+  removeBackground(img_unchanged, threshold, maxValue)
 
   cv2.waitKey(0)
   cv2.destroyAllWindows()
@@ -69,7 +69,6 @@ for i in range(0,len(dirImagesList)):
   GLOBAL THRESHOLDING EXPERIMENT -> DOENS'T WORK AT THIS STAGE
   LOOK FOR OTHER METHODS: Adaptive Mean Thresholding, Adaptive Gaussian Thresholding
   """
-
   cleaned_img = cv2.medianBlur(cleaned_img_gray,5)
   ret,th1 = cv2.threshold(cleaned_img,127,256,cv2.THRESH_BINARY)
   th2 = cv2.adaptiveThreshold(cleaned_img,256,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
@@ -81,6 +80,9 @@ for i in range(0,len(dirImagesList)):
     plt.title(titles[j])
     plt.xticks([]),plt.yticks([])
   plt.show()
+
+  
+  
   if (i > 0):
     exit(0) #Remove this if clause to let this script 
 
